19a73c8d4c50c6239a77d4f8afb75836b19603ff
Saumya Nayak <saumyo224@gmail.com>
2025-06-11 14:51:35 +0530

    verify-email page done

--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -1,12 +1,54 @@
+"use client";
+import axios from "axios";
 import React from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { useSearchParams } from "next/navigation";
 
 export default function VerifyEmailPage() {
+  const [token, setToken] = React.useState({
+    token: "",
+  });
+  const [loading, setloading] = React.useState(false);
+  const serchParams = useSearchParams();
+
+  React.useEffect(() => {
+    const extractedToken: string = serchParams.get("token") || "";
+    setToken({ token: extractedToken });
+  }, [serchParams]);
+
+  const sendToken = async () => {
+    try {
+      setloading(true);
+      console.log(token);
+      const response = await axios.post("/api/user/verify", token);
+      console.log(response);
+      if (response.data.status !== 200) {
+        toast.error(response.data.error);
+      } else {
+        toast.success("Email verified");
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      } else {
+        toast.error("Unexpected error occur");
+      }
+    } finally {
+      setloading(false);
+    }
+  };
   return (
     <div className="flex flex-col gap-5 min-h-screen justify-center items-center">
-      <h1>Click below to get verify</h1>
-      {/* <form action="" className="flex mt-5 flex-col gap-5">
-        <button className="bg-blue-500 hover:bg-blue-600 py-1 ">Verify</button>
-      </form> */}
+      <h1 className="text-3xl font-medium">
+        {loading ? "Processing" : "Click below to get verify"}
+      </h1>
+      <button
+        className="bg-blue-500 hover:bg-blue-600 py-2 px-7 rounded hover:cursor-pointer font-semibold"
+        onClick={sendToken}
+      >
+        Verify
+      </button>
+      <Toaster position="bottom-center" />
     </div>
   );
 }
